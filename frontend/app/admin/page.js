b025926a952dff85696fa2f1fbc766418278d786
courage97 <oyewolebarnabas97@gmail.com>
2026-06-27 05:22:41 -0700

    feat: Fix the error of usecallback in the admin route

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -1,7 +1,7 @@
 'use client';
 const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
 
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { toast, ToastContainer, Slide } from 'react-toastify';
